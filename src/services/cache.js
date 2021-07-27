80d37f4407045321c15b9a5b89fd312b99af6a48
James Roller, Jr <og.readmore@gmail.com>
2021-07-26 21:29:08 -0700

    feat: create cache key

--- src/services/cache.js
+++ src/services/cache.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 
 const { exec } = mongoose.Query.prototype;
 
 mongoose.Query.prototype.exec = function newExec(...args) {
-  console.log('IM ABOUT TO RUN A QUERY');
+  const key = ({ ...this.getQuery(), collection: this.mongooseCollection.name });
+  console.log(key);
+
   return exec.apply(this, args);
 };
